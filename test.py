6ea61207b78a82948a5c140d25ff509033209701
beathorx <oe.aoy1@gmail.com>
2023-03-21 06:13:39 -0700

    Add Grad Data

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import os
 import pickle
 
-path = os.path.join(os.getcwd(),"Districts_Dicts","NYC GEOG DIST # 2 - MANHATTAN .dict")
+path = os.path.join(os.getcwd(),"Grad_Rate_Dicts","NYC GEOG DIST # 1 - MANHATTAN .dict")
 
 dist_dict = {}
 
 with open(path, 'rb') as fp:
     dist_dict['test'] = pickle.load(fp)
 
-print(dist_dict)
+print(dist_dict['test']['BARD HIGH SCHOOL EARLY COLLEGE '])
